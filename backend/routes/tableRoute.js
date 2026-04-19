12b39acdf17e2e8311ed58a462982aab2239fa2f
RAZAFIMANDIMBY Zo Hery Niaina Jacky <zorazafi@CPPSYSTEM.LOCAL>
2026-04-19 15:05:07 +0300

    update files update cors

--- backend/routes/tableRoute.js
+++ backend/routes/tableRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { findAll,findById, isUserInTable } = require("../controllers/tablesController");
+const { findAll,findById, isUserInTable, getLastHistory } = require("../controllers/tablesController");
 const router = express.Router();
 
 router.get("/tables", findAll);
 router.get("/tables/:id", findById);
+router.get("/table/:id/last", getLastHistory);
 router.get("/tables/in-table/:userId", isUserInTable);
 
 module.exports = router;
